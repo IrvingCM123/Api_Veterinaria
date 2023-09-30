04f5e87f79177bb6b149112cd4d3d7caabaf3368
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2023-09-30 13:30:05 -0600

    agregar cors

--- app.ts
+++ app.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import productoRoutes from './router/routes'; 
 import { errorHandler } from './middleware/errorHandler';
-
+import cors from 'cors'; 
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use('/api', productoRoutes); 
 
