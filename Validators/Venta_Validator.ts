fd3365d76a51ff761e499965f4318c5f22d9ba75
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2023-09-30 17:45:59 -0600

    cambios

--- Validators/Venta_Validator.ts
+++ Validators/Venta_Validator.ts
@@ -1,40 +1,67 @@
-import { check, ValidationChain } from 'express-validator';
+import { check, ValidationChain } from "express-validator";
 
-// Definición de las validaciones para la estructura general de la venta
+/**
+ * Validador para la estructura general de la venta. Este validador se utiliza para validar
+ * los datos enviados en una solicitud de registro de venta antes de almacenarlos en la base de datos.
+ * Se asegura de que los datos cumplan con los requisitos específicos.
+ */
+
 export const validarVenta: ValidationChain[] = [
   // Validación para asegurar que "ProductosVendidos" sea un array válido y no esté vacío
-  check('ProductosVendidos')
-    .isArray().withMessage('Los productos vendidos deben ser un array válido')
+  check("ProductosVendidos")
+    .isArray()
+    .withMessage("Los productos vendidos deben ser un array válido")
     .custom((productos) => {
       if (!Array.isArray(productos) || productos.length === 0) {
-        throw new Error('Los productos vendidos deben ser un array válido y no estar vacíos');
+        throw new Error(
+          "Los productos vendidos deben ser un array válido y no estar vacíos"
+        );
       }
 
       // Validación individual de cada producto en "ProductosVendidos"
       productos.forEach((producto, index) => {
         // Verifica que cada elemento en "ProductosVendidos" sea un objeto
-        if (typeof producto !== 'object') {
-          throw new Error(`El producto en la posición ${index} no es un objeto válido`);
+        if (typeof producto !== "object") {
+          throw new Error(
+            `El producto en la posición ${index} no es un objeto válido`
+          );
         }
 
         // Verifica que cada producto tenga una propiedad 'Nombre'
-        if (!producto.hasOwnProperty('Nombre')) {
-          throw new Error(`El producto en la posición ${index} debe tener una propiedad 'Nombre'`);
+        if (!producto.hasOwnProperty("Nombre")) {
+          throw new Error(
+            `El producto en la posición ${index} debe tener una propiedad 'Nombre'`
+          );
         }
 
         // Verifica que el precio de cada producto sea un número válido
-        if (!producto.hasOwnProperty('Precio') || isNaN(Number(producto.Precio))) {
-          throw new Error(`El producto en la posición ${index} debe tener un precio numérico`);
+        if (
+          !producto.hasOwnProperty("Precio") ||
+          isNaN(Number(producto.Precio))
+        ) {
+          throw new Error(
+            `El producto en la posición ${index} debe tener un precio numérico`
+          );
         }
 
         // Verifica que la cantidad de cada producto sea un número válido
-        if (!producto.hasOwnProperty('Cantidad') || isNaN(Number(producto.Cantidad))) {
-          throw new Error(`El producto en la posición ${index} debe tener una cantidad numérica`);
+        if (
+          !producto.hasOwnProperty("Cantidad") ||
+          isNaN(Number(producto.Cantidad))
+        ) {
+          throw new Error(
+            `El producto en la posición ${index} debe tener una cantidad numérica`
+          );
         }
 
         // Verifica que el subtotal de cada producto sea un número válido
-        if (!producto.hasOwnProperty('Subtotal') || isNaN(Number(producto.Subtotal))) {
-          throw new Error(`El producto en la posición ${index} debe tener un subtotal numérico`);
+        if (
+          !producto.hasOwnProperty("Subtotal") ||
+          isNaN(Number(producto.Subtotal))
+        ) {
+          throw new Error(
+            `El producto en la posición ${index} debe tener un subtotal numérico`
+          );
         }
       });
 
@@ -42,17 +69,21 @@ export const validarVenta: ValidationChain[] = [
     }),
 
   // Validación para asegurar que "TotalVenta" sea un número válido
-  check('TotalVenta')
-    .isNumeric().withMessage('El total de la venta debe ser un número válido'),
+  check("TotalVenta")
+    .isNumeric()
+    .withMessage("El total de la venta debe ser un número válido"),
 
   // Validación para asegurar que "TotalProductosVendidos" sea un número válido
-  check('TotalProductosVendidos')
-    .isNumeric().withMessage('El total de productos vendidos debe ser un número válido'),
+  check("TotalProductosVendidos")
+    .isNumeric()
+    .withMessage("El total de productos vendidos debe ser un número válido"),
 
   // Validación para asegurar que "FechaVenta" tenga el formato "YYYY-MM-DD HH:mm:ss"
-  check('FechaVenta')
+  check("FechaVenta")
     .matches(/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/)
-    .withMessage('La fecha de la venta debe tener el formato "YYYY-MM-DD HH:mm:ss"'),
+    .withMessage(
+      'La fecha de la venta debe tener el formato "YYYY-MM-DD HH:mm:ss"'
+    ),
 ];
 
 /**
