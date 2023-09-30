81c6d436278386e845ef73a9f53dbfb95c26871f
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2023-09-29 22:13:35 -0600

    subir a Oktetot

--- database/firebase/serviceAccountKey.ts
+++ database/firebase/serviceAccountKey.ts
@@ -4,7 +4,7 @@ export const json = {
   type: process.env.type,
   project_id: process.env.project_id,
   private_key_id: process.env.private_key_id,
-  private_key: llave.private_key,
+  private_key: process.env.private_key,
   client_email: process.env.client_email,
   client_id: process.env.client_id,
   auth_uri: process.env.auth_uri,
