64d9cd12d5fd356c8ea511dfd5c1e2ee8aef1d12
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2023-10-07 15:21:32 -0600

    cambios

---
+++ controllers/provedores.Controller.ts
@@ -0,0 +1,45 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+// Controlador para el modelo CatalogoProveedor
+
+// Obtener todos los proveedores
+export async function getAllProveedores() {
+    return await prisma.catalogoProveedor.findMany();
+}
+
+// Obtener un proveedor por su ID
+export async function getProveedorById(id: number) {
+    return await prisma.catalogoProveedor.findUnique({
+        where: { id_proveedor: id },
+    });
+}
+
+// Crear un nuevo proveedor
+export async function createProveedor(nombre: string, nomenclatura: string) {
+    return await prisma.catalogoProveedor.create({
+        data: {
+            nombre,
+            nomenclatura,
+        },
+    });
+}
+
+// Actualizar un proveedor por su ID
+export async function updateProveedor(id: number, nombre: string, nomenclatura: string) {
+    return await prisma.catalogoProveedor.update({
+        where: { id_proveedor: id },
+        data: {
+            nombre,
+            nomenclatura,
+        },
+    });
+}
+
+// Eliminar un proveedor por su ID
+export async function deleteProveedor(id: number) {
+    return await prisma.catalogoProveedor.delete({
+        where: { id_proveedor: id },
+    });
+}
