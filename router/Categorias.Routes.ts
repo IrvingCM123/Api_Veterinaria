0abeb41091708cd8154358b89b741057b9b19368
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2023-10-22 13:47:52 -0600

    cambios

--- router/Categorias.Routes.ts
+++ router/Categorias.Routes.ts
@@ -2,6 +2,16 @@ import { Request, Response, NextFunction, Router } from "express";
 
 import * as CategoriasController from "../controllers/CatalogoCategoria/Categoria.Logic";
 
+import { handleValidationErrors } from "../middleware/Categoria/Categoria.Middleware";
+
+import {
+    validateCreateCategoria,
+    validateDeleteCategoria,
+    validateGetCategoriaById,
+    validateUpdateCategoria,
+    validateGetAllCategorias,
+} from "../Validators/CatalogoCategoria/Categoria.Validator";
+
 const router = Router();
 
 // Rutas para el controlador de categorias
@@ -11,17 +21,23 @@ const router = Router();
  *  @desc Get All Categorias
  *  @access Public
  *  @params null
+ *  @validation validateGetAllCategorias, handleValidationErrors
  *  @return json con todas las categorias
  */
 
-router.get("/", async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const categorias = await CategoriasController.getAllCategoriasController();
-        res.status(200).json(categorias);
-    } catch (error) {
-        next(error);
+router.get(
+    "/",
+    validateGetAllCategorias,
+    handleValidationErrors,
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const categorias =
+                await CategoriasController.getAllCategoriasController();
+            res.status(200).json(categorias);
+        } catch (error) {
+            next(error);
+        }
     }
-}
 );
 
 /**
@@ -29,18 +45,25 @@ router.get("/", async (req: Request, res: Response, next: NextFunction) => {
  *  @desc Get An Categoria
  *  @access Public
  *  @params id
+ *  @validation validateGetCategoriaById, handleValidationErrors
  *  @return json con la categoria solicitada
  */
 
-router.get("/:id", async (req: Request, res: Response, next: NextFunction) => {
-    const id = parseInt(req.params.id, 10);
-    try {
-        const categoria = await CategoriasController.getCategoriaByIdController(id);
-        res.status(200).json(categoria);
-    } catch (error) {
-        next(error);
+router.get(
+    "/:id",
+    validateGetCategoriaById,
+    handleValidationErrors,
+    async (req: Request, res: Response, next: NextFunction) => {
+        const id = parseInt(req.params.id, 10);
+        try {
+            const categoria = await CategoriasController.getCategoriaByIdController(
+                id
+            );
+            res.status(200).json(categoria);
+        } catch (error) {
+            next(error);
+        }
     }
-}
 );
 
 /**
@@ -48,18 +71,26 @@ router.get("/:id", async (req: Request, res: Response, next: NextFunction) => {
  *  @desc Create An Categoria
  *  @access Public
  *  @params -nombre: string, -nomenclatura: string
+ *  @validation validateCreateCategoria, handleValidationErrors
  *  @return json con la categoria creada
  */
 
-router.post("/", async (req: Request, res: Response, next: NextFunction) => {
-    const { nombre, nomenclatura } = req.body;
-    try {
-        const categoria = await CategoriasController.createCategoriaController(nombre, nomenclatura);
-        res.status(200).json(categoria);
-    } catch (error) {
-        next(error);
+router.post(
+    "/",
+    validateCreateCategoria,
+    handleValidationErrors,
+    async (req: Request, res: Response, next: NextFunction) => {
+        const { nombre, nomenclatura } = req.body;
+        try {
+            const categoria = await CategoriasController.createCategoriaController(
+                nombre,
+                nomenclatura
+            );
+            res.status(200).json(categoria);
+        } catch (error) {
+            next(error);
+        }
     }
-}
 );
 
 /**
@@ -67,19 +98,28 @@ router.post("/", async (req: Request, res: Response, next: NextFunction) => {
  *  @desc Update An Categoria
  *  @access Public
  *  @params id, -nombre: string, -nomenclatura: string
+ *  @validation validateUpdateCategoria, handleValidationErrors
  *  @return json con la categoria actualizada
  */
 
-router.put("/:id", async (req: Request, res: Response, next: NextFunction) => {
-    const id = parseInt(req.params.id, 10);
-    const { nombre, nomenclatura } = req.body;
-    try {
-        const categoria = await CategoriasController.updateCategoriaController(id, nombre, nomenclatura);
-        res.status(200).json(categoria);
-    } catch (error) {
-        next(error);
+router.put(
+    "/:id",
+    validateUpdateCategoria,
+    handleValidationErrors,
+    async (req: Request, res: Response, next: NextFunction) => {
+        const id = parseInt(req.params.id, 10);
+        const { nombre, nomenclatura } = req.body;
+        try {
+            const categoria = await CategoriasController.updateCategoriaController(
+                id,
+                nombre,
+                nomenclatura
+            );
+            res.status(200).json(categoria);
+        } catch (error) {
+            next(error);
+        }
     }
-}
 );
 
 /**
@@ -87,18 +127,25 @@ router.put("/:id", async (req: Request, res: Response, next: NextFunction) => {
  *  @desc Delete An Categoria
  *  @access Public
  *  @params id
+ *  @validation validateDeleteCategoria, handleValidationErrors
  *  @return json con la categoria eliminada
  */
 
-router.delete("/:id", async (req: Request, res: Response, next: NextFunction) => {
-    const id = parseInt(req.params.id, 10);
-    try {
-        const categoria = await CategoriasController.deleteCategoriaController(id);
-        res.status(200).json(categoria);
-    } catch (error) {
-        next(error);
+router.delete(
+    "/:id",
+    validateDeleteCategoria,
+    handleValidationErrors,
+    async (req: Request, res: Response, next: NextFunction) => {
+        const id = parseInt(req.params.id, 10);
+        try {
+            const categoria = await CategoriasController.deleteCategoriaController(
+                id
+            );
+            res.status(200).json(categoria);
+        } catch (error) {
+            next(error);
+        }
     }
-}
 );
 
 export default router;
