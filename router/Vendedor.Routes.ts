7ddc82dad95fb69ee6fa0261eee429365c6914cf
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2023-10-22 17:41:29 -0600

    cambios

--- router/Vendedor.Routes.ts
+++ router/Vendedor.Routes.ts
@@ -2,6 +2,16 @@ import { Request, Response, NextFunction, Router } from "express";
 
 import * as VendedorController from "../controllers/Vendedor/Vendedores.Logic";
 
+import { handleValidationErrors } from "../middleware/Vendedor/Vendedor.Middleware";
+
+import {
+    validateObtenerTodosLosVendedores,
+    validateObtenerVendedorPorId,
+    validateCrearNuevoVendedor,
+    validateActualizarVendedor,
+    validateEliminarVendedor
+} from "../Validators/Vendedores/Vendedor.Validator";
+
 const router = Router();
 
 // Rutas para el controlador de vendedores
@@ -11,17 +21,21 @@ const router = Router();
  *  @desc Get All Vendedores
  *  @access Public
  *  @params null
+ *  @validation validateObtenerTodosLosVendedores, handleValidationErrors
  *  @return json con todos los vendedores
  */
 
-router.get("/", async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const vendedores = await VendedorController.obtenerTodosLosVendedores();
-        res.status(200).json(vendedores);
-    } catch (error) {
-        next(error);
+router.get("/",
+    validateObtenerTodosLosVendedores,
+    handleValidationErrors,
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const vendedores = await VendedorController.obtenerTodosLosVendedores();
+            res.status(200).json(vendedores);
+        } catch (error) {
+            next(error);
+        }
     }
-}
 );
 
 /**
@@ -29,18 +43,22 @@ router.get("/", async (req: Request, res: Response, next: NextFunction) => {
  *  @desc Get An Vendedor
  *  @access Public
  *  @params id
+ *  @validation validateObtenerVendedorPorId, handleValidationErrors
  *  @return json con el vendedor solicitado
  */
 
-router.get("/:id", async (req: Request, res: Response, next: NextFunction) => {
-    const id = parseInt(req.params.id, 10);
-    try {
-        const vendedor = await VendedorController.obtenerVendedorPorId(id);
-        res.status(200).json(vendedor);
-    } catch (error) {
-        next(error);
+router.get("/:id",
+    validateObtenerVendedorPorId,
+    handleValidationErrors,
+    async (req: Request, res: Response, next: NextFunction) => {
+        const id = parseInt(req.params.id, 10);
+        try {
+            const vendedor = await VendedorController.obtenerVendedorPorId(id);
+            res.status(200).json(vendedor);
+        } catch (error) {
+            next(error);
+        }
     }
-}
 );
 
 /**
@@ -48,18 +66,22 @@ router.get("/:id", async (req: Request, res: Response, next: NextFunction) => {
  *  @desc Create An Vendedor
  *  @access Public
  *  @params -acronimo: string, -permisoVenta: boolean, -userId: number
+ *  @validation validateCrearNuevoVendedor, handleValidationErrors
  *  @return json con el vendedor creado
  */
 
-router.post("/", async (req: Request, res: Response, next: NextFunction) => {
-    const { acronimo, permisoVenta, userId } = req.body;
-    try {
-        const vendedor = await VendedorController.crearNuevoVendedor(acronimo, permisoVenta, userId);
-        res.status(200).json(vendedor);
-    } catch (error) {
-        next(error);
+router.post("/",
+    validateCrearNuevoVendedor,
+    handleValidationErrors,
+    async (req: Request, res: Response, next: NextFunction) => {
+        const { acronimo, permisoVenta, userId } = req.body;
+        try {
+            const vendedor = await VendedorController.crearNuevoVendedor(acronimo, permisoVenta, userId);
+            res.status(200).json(vendedor);
+        } catch (error) {
+            next(error);
+        }
     }
-}
 );
 
 /**
@@ -67,19 +89,23 @@ router.post("/", async (req: Request, res: Response, next: NextFunction) => {
  *  @desc Update An Vendedor
  *  @access Public
  *  @params id, -acronimo: string, -permisoVenta: boolean, -userId: number
+ *  @validation validateActualizarVendedor, handleValidationErrors
  *  @return json con el vendedor actualizado
  */
 
-router.put("/:id", async (req: Request, res: Response, next: NextFunction) => {
-    const id = parseInt(req.params.id, 10);
-    const { acronimo, permisoVenta, userId } = req.body;
-    try {
-        const vendedor = await VendedorController.actualizarVendedor(id, acronimo, permisoVenta, userId);
-        res.status(200).json(vendedor);
-    } catch (error) {
-        next(error);
+router.put("/:id",
+    validateActualizarVendedor,
+    handleValidationErrors,
+    async (req: Request, res: Response, next: NextFunction) => {
+        const id = parseInt(req.params.id, 10);
+        const { acronimo, permisoVenta, userId } = req.body;
+        try {
+            const vendedor = await VendedorController.actualizarVendedor(id, acronimo, permisoVenta, userId);
+            res.status(200).json(vendedor);
+        } catch (error) {
+            next(error);
+        }
     }
-}
 );
 
 /**
@@ -87,18 +113,22 @@ router.put("/:id", async (req: Request, res: Response, next: NextFunction) => {
  *  @desc Delete An Vendedor
  *  @access Public
  *  @params id
+ *  @validation validateEliminarVendedor, handleValidationErrors
  *  @return json con el vendedor eliminado
  */
 
-router.delete("/:id", async (req: Request, res: Response, next: NextFunction) => {
-    const id = parseInt(req.params.id, 10);
-    try {
-        const vendedor = await VendedorController.eliminarVendedor(id);
-        res.status(200).json(vendedor);
-    } catch (error) {
-        next(error);
+router.delete("/:id",
+    validateEliminarVendedor,
+    handleValidationErrors,
+    async (req: Request, res: Response, next: NextFunction) => {
+        const id = parseInt(req.params.id, 10);
+        try {
+            const vendedor = await VendedorController.eliminarVendedor(id);
+            res.status(200).json(vendedor);
+        } catch (error) {
+            next(error);
+        }
     }
-}
 );
 
 export default router;
