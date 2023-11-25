0d2de6b5082395d90695b7f43239ef0d58c6ffb6
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2023-11-24 19:23:14 -0600

    cambios

--- dist/controllers/Venta/Ventas.Logic.js
+++ dist/controllers/Venta/Ventas.Logic.js
@@ -174,10 +174,8 @@ function obtenerInformacionReporte(año, mes) {
             ventasPorMes[i].detallesVenta = detallesVenta;
         }
         for (let i = 0; i < ventasPorMes.length; i++) {
-            console.log(ventasPorMes[i].subtotal);
             ventasPorMes[i].subtotal = parseFloat(ventasPorMes[i].subtotal).toFixed(2);
             for (let j = 0; j < ventasPorMes[i].detallesVenta.length; j++) {
-                console.log(ventasPorMes[i].detallesVenta[j].subtotal);
                 ventasPorMes[i].detallesVenta[j].subtotal = parseFloat(ventasPorMes[i].detallesVenta[j].subtotal).toFixed(2);
             }
         }
